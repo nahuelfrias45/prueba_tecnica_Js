b18dd190c7dcd0161e698d0b7fd3f48f74c5f684
nahuelfrias45 <nahuelfrias@outlook.com.ar>
2023-08-04 16:36:48 -0300

    agrego sección de marcadores/ termino todas las funcionalidades
    agrego un sector abajo del form donde se van a ir agregando los marcadores, con un botón que te dirige a ese punto del mapa agregado

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+const map = L.map('map');//indico en donde se va a mostrar el mapa (en este caso en el div map)
+const mapGlobal = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);// establesco el mapa a utilizar
+
+const form = document.getElementById('form');
+const marcadorDatos = [];
+
+map.doubleClickZoom.disable()
+
+// recupero una cordenada cerca del usuario y la uso como posición default
+const posicionDefault = navigator.geolocation.getCurrentPosition(
+    (pos) => {
+        const { coords } = pos
+        map.setView([coords.latitude, coords.longitude], 15)
+    },
+    (err) => {
+        console.log(err)
+    },
+    {
+        enableHighAcurracy:true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+)
+
+//Leo el archivo JSON, recupero la informacion y creo marcadores con los datos 
 const xhttp = new XMLHttpRequest();
 xhttp.open('GET', 'coordenadas.json', true);
 xhttp.send();
@@ -12,38 +39,13 @@ xhttp.onreadystatechange = function(){
                 lng: arr[1]
             }
             let marker = L.marker(posicion).addTo(map);
-            marker.bindPopup(`<b>Nombre: ${coords.nombre}</b><br><b>Dirección: ${coords.direccion}</b><br><b>Teléfono: ${coords.telefono}</b><br><b>Categoria: ${coords.categoria}</b><br><b>(x , Y): ${coords.coordenadas}</b><br><button id="btn-eliminar" data-id="${coords.coordenadas}" onclick="eliminar()">Eliminar</button>`)
-            console.log(posicion)
+            marker.bindPopup(`<b>Nombre: ${coords.nombre}</b><br><b>Dirección: ${coords.direccion}</b><br><b>Teléfono: ${coords.telefono}</b><br><b>Categoria: ${coords.categoria}</b><br><b>(x , Y): ${coords.coordenadas}</b><br><button id="btn-eliminar" data-name="${coords.nombre}" data-id="${coords.coordenadas}" onclick="eliminar()">Eliminar</button>`)
         });
         
     }
 }
 
-const marcadorDatos = [];
-const map = L.map('map');//indico en donde se va a mostrar el mapa (en este caso en el div map)
-const mapGlobal = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19
-}).addTo(map);// establesco el mapa a utilizar
-const agregar = document.getElementById('btn-agregar');
-
-// recupero una cordenada cerca del usuario y la uso como posición default
-const firstPosition = navigator.geolocation.getCurrentPosition(
-    (pos) => {
-        const { coords } = pos
-        console.log(coords)
-        map.setView([coords.latitude, coords.longitude], 16)
-    },
-    (err) => {
-        console.log(err)
-    },
-    {
-        enableHighAcurracy:true,
-        timeout: 5000,
-        maximumAge: 0
-    }
-)
-
-agregar.addEventListener('click', function(event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault()
     let nombre = document.getElementById('nombre').value;
     let direccion = document.getElementById('direccion').value;
@@ -55,28 +57,51 @@ agregar.addEventListener('click', function(event) {
         telefono = "no tiene numero"
     }// llenamos el campo de telefono si este mismo no es completado
 
-    console.log(nombre)
-    console.log(direccion)
-    console.log(telefono)
-    console.log(coordenadas)
-    console.log(categoria)
-
     let arr = coordenadas.split(",")// utilizo split para separar las coordenadas por su " , " que vuelven como strings 
     let posicion = {
         lat: arr[0],
         lng: arr[1]
     }//la libreria para marcar una posicion busca en un objeto las keys 'lat' y 'lng'
     let marker = L.marker(posicion).addTo(map);
-    marker.bindPopup(`<b>Nombre: ${nombre}</b><br><b>Dirección: ${direccion}</b><br><b>Teléfono: ${telefono}</b><br><b>Categoria: ${categoria}</b><br><b>(x , Y): ${coordenadas}</b><br><button id="btn-eliminar" data-id="${coordenadas}" onclick="eliminar()">Eliminar</button>`)
+    marker.bindPopup(`<b>Nombre: ${nombre}</b><br><b>Dirección: ${direccion}</b><br><b>Teléfono: ${telefono}</b><br><b>Categoria: ${categoria}</b><br><b>(x , Y): ${coordenadas}</b><br><button id="btn-eliminar" data-name="${nombre}" data-id="${coordenadas}" onclick="eliminar()">Eliminar</button>`)
 
     map.flyTo(posicion, 18)//te traslada al lugar del marcador agregado
 
-    marcadorDatos.push({nombre: nombre, direccion: direccion, telefono: telefono, categoria: categoria, coordenadas: coordenadas})
-    console.log(marcadorDatos)
+    marcadorDatos.push({nombre: nombre, coordenadas: coordenadas, direccion: direccion})// guardo los marcadores creado por el form en un array
+    
+    let marcadoresHTML = "";
+    marcadorDatos.forEach(marcadorD => {
+        let marcador = marcadorD.nombre;
+        let coordenadas = marcadorD.coordenadas;
+        let direccion = marcadorD.direccion;
+        marcadoresHTML += `<div id="${marcador}" class="contenedor-m"><span><i class="bi bi-geo-alt" id="moverMapa" onclick="mover()" data-id="${coordenadas}"></i><b>${marcador}</b> (${direccion})</span></div>`;
+        let marcadores = document.getElementById('marcadores');
+        marcadores.innerHTML = marcadoresHTML;
+    });
+
+    //limpio los inputs
+    document.getElementById('nombre').value = "";
+    document.getElementById('direccion').value = "";
+    document.getElementById('telefono').value = "";
+    document.getElementById('coordenadas').value = "";
+    document.getElementById('categoria').value = "";
 })
+
+function mover(){
+    let btnMover = document.getElementById('moverMapa')
+    let coordenadas = btnMover.dataset.id
+    let arr = coordenadas.split(",")
+    let posicion = {
+        lat: arr[0],
+        lng: arr[1]
+    }
+    map.flyTo(posicion, 18)
+}
 
 function eliminar() {
     let btnEliminar = document.getElementById('btn-eliminar')
+    let divId = document.getElementById(btnEliminar.dataset.name)
+    let dataName = btnEliminar.dataset.name
     let coordenadas = btnEliminar.dataset.id
     let arr = coordenadas.split(",")
     let latitud = parseFloat(arr[0]);//paso las cordenadas a float 
@@ -89,17 +114,28 @@ function eliminar() {
             let marcadorLongitud = marcador.getLatLng().lng;
 
             if (marcadorLatitud === latitud && marcadorLongitud === longitud) {
-                // Remover el marcador del mapa
                 map.removeLayer(marcador);
             }
         }
     });
+
+    divId.remove()
+    console.log(divId)
 }
 
-map.doubleClickZoom.disable()
+
 
 map.on('dblclick', e => {
     let latLng = map.mouseEventToLatLng(e.originalEvent)
-    L.marker(latLng).addTo(map)
-    console.log(latLng)
+    let marker = L.marker(latLng).addTo(map)
+
+    //adapto el objeto de la variable latLng para que pueda ser utilizada en la funcion eliminar()
+    let markerLat = `${latLng.lat}`
+    let markerLng = `${latLng.lng}`
+    let markerString = markerLat+","+markerLng
+
+    //la funcion eliminar() lee el string del data-id y busca una " , " para hacer el split
+    marker.bindPopup(`<button id="btn-eliminar" data-id="${markerString}" onclick="eliminar()">Eliminar</button>`)
 })
+
+  
